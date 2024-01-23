b46377e18517fe2eb651c15cd1182e253001d446
Juhana Autti <juhana.autti@gmail.com>
2024-01-23 11:57:38 +0200

    Add more helpers

--- aoch/decorators.py
+++ aoch/decorators.py
@@ -1,16 +1,16 @@
-from time import perf_counter
 def timer(func):
-	'''Decorator that prints the execution time'''
-	
-	def wrap(*args, **kwargs):
-		start = perf_counter()
-		result = func(*args, **kwargs)
-		end = perf_counter()
+    '''Decorator that prints the execution time'''
+    from time import perf_counter
 
-		print(f"{(end - start )* 1000} ms")
-		
-		return result
-	return wrap
+    def wrap(*args, **kwargs):
+        start = perf_counter()
+        result = func(*args, **kwargs)
+        end = perf_counter()
+
+        print(f"{(end - start )* 1000} ms")
+        
+        return result
+    return wrap
 
 def log_result(func, log=print):
 	'''Decorator that prints the result of the executed function'''
--- aoch/helpers.py
+++ aoch/helpers.py
@@ -9,9 +9,30 @@ def transpose(matrix: list[list[int]]) -> list[list[int]]:
 			result[c][r] = matrix[r][c]
 	return result
 
+def binarySearch(arr, target):
+	lo, hi = 0, len(arr) - 1
+	while lo <= hi:
+		m = (lo + hi) // 2
+		if arr[m] < target:
+			lo = m + 1
+		elif arr[m] > target:
+			hi = m - 1
+		else:
+			return m
+	return -1
+
 def adjacent(pos):
     for axis in range(len(pos)):
         for d in (-1, 1):
             q = list(pos)
             q[axis] += d
             yield tuple(q)
+
+def neighbors(pos):
+	from itertools import product
+	dimensions = len(pos)
+	combinations = product([-1, 0, 1], repeat=dimensions)
+	for it in combinations:
+		if not any(it):
+			continue
+		yield tuple(sum(i) for i in zip(it, pos))
