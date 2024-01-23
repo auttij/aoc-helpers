284db50929f4b6add2140927aea0535876964a80
Juhana Autti <juhana.autti@gmail.com>
2024-01-23 09:52:08 +0200

    Add basic structure

---
+++ aoc-helpers/aoc-helpers.py
@@ -0,0 +1,9 @@
+def transpose(matrix):
+	rows = len(matrix)
+	column = len(matrix[0])
+	result = [[0 for i in range(rows)] for j in range(column)]
+
+	for r in range(rows):
+		for c in range(column):
+			result[c][r] = matrix[r][c]
+	return result
